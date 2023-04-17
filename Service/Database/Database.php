807bf5f2b262512d2ada43cbadaf2d3800ca94d5
Charlie Hills <github.cb25@tmx.email>
2023-04-17 01:02:04 -0500

    Add incomplete (and unused) Database layer
    This can connect to a database and select data from a table, but
    that's it. It's also not used at this point, since the information
    ultimately intended for the database is hardcoded into PHP data
    structures directly in the engine. This will be temporary, while
    the basic engine is being built.

---
+++ Service/Database/Database.php
@@ -0,0 +1,34 @@
+<?php namespace Service\Database;
+
+class Database
+{
+    private $dbh;
+
+    public function __construct()
+    {
+
+    }
+
+    public function connect(string $host, string $username, ?string $password, string $database)
+    {
+        try {
+            $this->dbh = new \PDO("mysql:host=$host;dbname=$database", $username, $password);
+        } catch (PDOException $e) {
+            print "Error connecting to database: " . $e->getMessage() . "\n";
+            die();
+        }
+    }
+
+    public function select(string $query)
+    {
+        $rows = [];
+
+        foreach ($this->dbh->query($query) as $row) {
+            $rows[] = $row;
+        }
+
+        return $rows;
+    }
+
+
+}
