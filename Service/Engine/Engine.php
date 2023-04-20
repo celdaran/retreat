f288506a22aed7628aed7e58ac44f6283563f867
Charlie Hills <github.cb25@tmx.email>
2023-04-20 09:06:20 -0500

    Commit massive rewrite of engine
    Before, income was a fixed amount per period drawn across all active
    income sources until the monthly amount was met. After that, the
    expenses followed suit. And after each period, I had either a
    surplus or a deficit.
    
    But this made no sense in reality. The entire point of Retreat is
    to take a pile of money and figure out how long it will last. And
    I don't do that by pre-determining my monthly income. In this new
    version, I reversed the polarity. First, add up all monthly expenses
    and THEN draw that exact amount from all active income sources.
    
    It's a lot more complicated than before but waaaaay better. In this
    way I can: 1) define all my expected expenses, 2) write down every
    source of income, and 3) click *run* to see how long it works.
    
    Additional details:
    
    * Uses new Log service
    * Keeps an internal audit of expenses and income, by period
    * Calculates interest across all assets, not just active
    * Improved calculateInterest() function
    * Plus general improvements and bug fixes too numerous to mention

--- Service/Engine/Engine.php
+++ Service/Engine/Engine.php
@@ -1,24 +1,35 @@
 <?php namespace Service\Engine;
 
-use \Service\Data\Income;
 use \Service\Data\Expense;
+use \Service\Data\Income;
+use \Service\Log;
 
 class Engine
 {
     private $plan = [];
+    private $audit = [];
 
-    private $income = [];
     private $expense = [];
+    private $income = [];
 
-    private $max_withdrawal = 5000;
+    private $log;
+
+    private $currentPeriod = 1;
 
     public function __construct()
     {
-        $income = new Income();
         $expense = new Expense();
+        $income = new Income();
 
-        $this->income = $income->getIncome();
         $this->expense = $expense->getExpense();
+        $this->income = $income->getIncome();
+        $this->log = new Log();
+        $this->log->setLevel('OFF');
+
+        $this->audit = [
+            'expense' => [],
+            'income' => [],
+        ];
     }
 
     /**
@@ -31,26 +42,16 @@ class Engine
         $month = $startMonth;
 
         // Loop until the requested number of months have passed.
-        // What will fundamentally happen here is that for each and every month,
-        // there will be income and expenses. That's it. The job of the engine,
-        // then, is to pull income from n-number of sources, figure out how much
-        // to spend each month. The source(s) will have their balances reduced
-        // each month based on expenses. The source(s) will have their balances
-        // increased each month based on investment gains.
+        for ($this->currentPeriod = 1; $this->currentPeriod <= $months; $this->currentPeriod++) {
 
-        for ($i = 1; $i <= $months; $i++) {
-
-            $income = $this->getIncomeForPeriod($year, $month);
             $expense = $this->getExpenseForPeriod($year, $month);
+            $this->adjustIncomeForPeriod($year, $month, $expense);
 
-            // Todo: reinvest monthly surplus into an account somewhere?
             $this->plan[] = [
-                'period' => $i,
+                'period' => $this->currentPeriod,
                 'year' => $year,
                 'month' => $month,
-                'income' => $income,
                 'expense' => $expense,
-                'surplus' => $income - $expense,
             ];
 
             if ($month % 12 === 0) {
@@ -60,7 +61,6 @@ class Engine
 
             $month++;
         }
-
     }
 
     /**
@@ -69,7 +69,7 @@ class Engine
     public function render()
     {
         foreach ($this->plan as $p) {
-            printf("%03d,%4d-%02d,+%d,-%d,%d\n", $p['period'], $p['year'], $p['month'], $p['income'], $p['expense'], $p['surplus']);
+            printf("%03d,%4d-%02d,%d\n", $p['period'], $p['year'], $p['month'], $p['expense']);
         }
 
         foreach ($this->income as $i) {
@@ -77,86 +77,29 @@ class Engine
             printf("  Current balance: \$%0.2f\n", $i['current_balance']);
             printf("\n");
         }
+
+        foreach ($this->audit['expense'] as $e) {
+            printf("%03d,%4d-%02d,%s,%d,%s\n",
+                $e['period'], $e['year'], $e['month'],
+                $e['name'], $e['amount'], $e['status'],
+            );
+        }
+
+        foreach ($this->audit['income'] as $i) {
+            printf("%03d,%4d-%02d,%s,%0.2f,%0.2f,%0.2f,%s\n", 
+                $i['period'], $i['year'], $i['month'], 
+                $i['name'], $i['opening_balance'], $i['current_balance'], $i['max_withdrawal'], $i['status']);
+        }
     }
 
     //------------------------------------------------------------------
     // Private functions
     //------------------------------------------------------------------
 
-    private function getIncomeForPeriod(int $year, int $month)
-    {
-        // Activate withdrawals based on current period
-        $i = 0;
-        foreach ($this->income as $income) {
-            // First, see if there's enough money left
-            if ($income['current_balance'] < $income['monthly_withdrawal']) {
-                $this->income[$i]['status'] = 'depleted';
-            }
-            // If we hit an untapped income, see if it's time to tap it
-            if ($income['status'] === 'untapped') {
-                // Two ways to tap!
-                // 1. It can follow a previously-depleted source
-                if ($income['begin']['after'] !== null) {
-                    if ($this->income[$income['begin']['after']]['status'] === 'depleted') {
-                        print("***DEBUG: Activating income source $i, in year $year month $month, after previous source depleted\n");
-                        $this->income[$i]['status'] = 'active';
-                    }
-                }
-                // 2. It can begin during a specified year and month
-                if ($income['begin']['after'] === null) {
-                    if (($year >= $income['begin']['year']) && ($month >= $income['begin']['month'])) {
-                        print("***DEBUG: Activating income source $i, in year $year month $month, as planned from the start\n");
-                        $this->income[$i]['status'] = 'active';
-                    }
-                }
-            }
-            $i++;
-        };
-
-        // Now get amounts, drawing from every eligible income source
-        $total = 0;
-        $i = 0;
-        foreach ($this->income as $income) {
-            // Check monthly withdrawl cap
-            if ($total <= $this->max_withdrawal) {
-
-                // Source must be active
-                if ($income['status'] === 'active') {
-
-                    // Check balance
-                    if ($income['current_balance'] >= $income['monthly_withdrawal']) {
-                        // Withdraw specified amount
-                        $total += $income['monthly_withdrawal'];
-                        // Reduce balance by withdrawal amount
-                        $this->income[$i]['current_balance'] -= $income['monthly_withdrawal'];
-                        // Increase balance by interest rate
-                        $this->income[$i]['current_balance'] += $this->calculateInterest(
-                            $this->income[$i]['current_balance'],
-                            $this->income[$i]['apr'],
-                            12,
-                            1/12,
-                        );
-                    } else {
-                        $this->income[$i]['status'] = 'depleted';
-                    }
-                }
-            }
-            $i++;
-        }
-
-        return $total;
-    }
-
-    private function calculateInterest(float $p, float $k, int $m, float $n)
-    {
-        // P*(1+(k/m))^(m*n)
-        return ($p * (1 + ($k / $m)) ** ($m * $n)) - $p;
-    }
-
     /**
      * Get expenses for a given period
      */
-    private function getExpenseForPeriod(int $year, int $month)
+    private function getExpenseForPeriod(int $year, int $month): float
     {
         // Activate expenses based on current period
         $i = 0;
@@ -164,7 +107,7 @@ class Engine
             // If we hit a planned expense, see if it's time to activate it
             if ($expense['status'] === 'planned') {
                 if (($year >= $expense['begin']['year']) && ($month >= $expense['begin']['month'])) {
-                    print("***DEBUG: Activating expense $i, in year $year month $month, as planned from the start\n");
+                    $this->log->debug("Activating expense $i, in year $year month $month, as planned from the start");
                     $this->expense[$i]['status'] = 'active';
                 }
             }
@@ -172,9 +115,17 @@ class Engine
         };
 
         // Now get amounts, drawing from every participating expense
-        $total = 0;
+        $total = 0.00;
         $i = 0;
         foreach ($this->expense as $expense) {
+            $this->audit['expense'][] = [
+                'period' => $this->currentPeriod,
+                'year' => $year,
+                'month' => $month,
+                'name' => $expense['name'],
+                'amount' => $expense['amount'],
+                'status' => $expense['status'],
+            ];
             if ($expense['status'] === 'active') {
                 $total += $expense['amount'];
             }
@@ -186,15 +137,158 @@ class Engine
         foreach ($this->expense as $expense) {
             if ($expense['status'] === 'active') {
                 if (($year >= $expense['end']['year']) && ($month >= $expense['end']['month'])) {
-                    print("***DEBUG: Ending expense $i, in year $year month $month, as planned from the start\n");
+                    $this->log->debug("Ending expense $i, in year $year month $month, as planned from the start");
                     $this->expense[$i]['status'] = 'ended';
                 }
             }
             $i++;
         } 
 
-        return $total;
+        return round($total, 2);
     }
 
+    /**
+     * Adjust income sources per period
+     * This is two passes:
+     * 1) reducing one or more balances per the $expense per period
+     * 2) increasing all balances to account for interest earned
+     */
+    private function adjustIncomeForPeriod(int $year, int $month, float $expense)
+    {
+        $this->makeWithdrawals($year, $month, $expense);
+        $this->earnInterest();
+    }
+
+    /**
+     * Withdraw money from fund(s) until expense is matched
+     */
+    private function makeWithdrawals(int $year, int $month, float $expense)
+    {
+        $total = 0;
+
+        foreach ($this->income as $income) {
+            $this->audit['income'][] = [
+                'period' => $this->currentPeriod,
+                'year' => $year,
+                'month' => $month,
+                'name' => $income['name'],
+                'opening_balance' => $income['opening_balance'],
+                'current_balance' => $income['current_balance'],
+                'max_withdrawal' => $income['max_withdrawal'],
+                'status' => $income['status'],
+            ];
+        }
+
+        for ($i = 0; $i < count($this->income); $i++) {
+
+            $this->activateSources($year, $month);
+
+            if ($this->income[$i]['status'] === 'active') {
+
+                // Set withdrawal amount
+                $amount = round(min(
+                    $expense,                               // When the full expense can be pulled from the source
+                    $this->income[$i]['max_withdrawal'],    // If there's a max, cap this period's withdrawal
+                    $this->income[$i]['current_balance'],   // Sometimes the current balance is the most we can pull
+                    ($expense - $total),                    // And sometimes it's just the small amount we need
+                ), 2);
+
+                if ($amount === 0.00) {
+                    $this->log->debug("Got a withdrawal amount near zero while pulling from source $i");
+                    $this->log->debug("  Amount          = $amount");
+                    $this->log->debug("  Target Expense  = $expense");
+                    $this->log->debug("  Max Withdrawal  = " . $this->income[$i]['max_withdrawal']);
+                    $this->log->debug("  Current Balance = " . $this->income[$i]['current_balance']);
+                    $this->log->debug("  Top-Off amount  = " . ($expense - $total));
+                    exit;
+                }
+                $this->log->debug("Pulling $amount to meet $expense from income source $i in $year-$month");
+                $total = round($total + $amount, 2);
+
+                // Reduce balance by withdrawal amount
+                $this->income[$i]['current_balance'] -= $amount;
+
+                // If we just depleted this source, activate the next one(s)
+                if ($this->income[$i]['current_balance'] <= 0) {
+                    $this->income[$i]['current_balance'] = 0;
+                    $this->income[$i]['status'] = 'depleted';
+                }
+
+                $this->log->debug("Current balance of income $i is " . $this->income[$i]['current_balance']);
+
+                if ($total === $expense) {
+                    // Just hack our way out of this
+                    break;
+                }
+            }
+        }
+
+        if ($total < $expense) {
+            $this->log->warn("Could not find enough money $total in $year-$month to cover expense $expense");
+        }
+
+        return;
+    }
+
+    /**
+     * Activate income sources per plan
+     */
+    private function activateSources(int $year, int $month)
+    {
+        // Activate withdrawals based on current period
+        for ($i = 0; $i < count($this->income); $i++) {
+            // If we hit an untapped income, see if it's time to tap it
+            if ($this->income[$i]['status'] === 'untapped') {
+                // Two ways to tap!
+                // 1. It can follow a previously-depleted source
+                if ($this->income[$i]['begin']['after'] !== null) {
+                    if ($this->income[$this->income[$i]['begin']['after']]['status'] === 'depleted') {
+                        $this->log->debug("Activating income source $i, in year $year month $month, after previous source depleted");
+                        $this->income[$i]['status'] = 'active';
+                    }
+                }
+                // 2. It can begin during a specified year and month
+                if ($this->income[$i]['begin']['after'] === null) {
+                    if (($year >= $this->income[$i]['begin']['year']) && ($month >= $this->income[$i]['begin']['month'])) {
+                        $this->log->debug("Activating income source $i, in year $year month $month, as planned from the start");
+                        $this->income[$i]['status'] = 'active';
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Loop through each income source and add interest
+     */
+    private function earnInterest()
+    {
+        for ($i = 0; $i < count($this->income); $i++) {
+            if ($this->income[$i]['current_balance'] > 0) {
+                $this->income[$i]['current_balance'] += $this->calculateInterest(
+                    $this->income[$i]['current_balance'],
+                    $this->income[$i]['apr']
+                );
+            }
+        }
+    }
+
+    /**
+     * Calculate compound interest
+     */
+    private function calculateInterest(float $p, float $r): float
+    {
+        // Convert rate
+        $r = $r / 100;
+
+        // Set time
+        $t = 1 / 12;
+
+        // Calculate new value
+        $v = $p * (1 + $r / 12) ** (12 * $t);
+
+        // Return *just* the interest
+        return round($v - $p, 2);
+    }
 
 }
