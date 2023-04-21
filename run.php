bad55d2b1b02b7ac8c4d1402d1c6f00801fa148d
Charlie Hills <github.cb25@tmx.email>
2023-04-21 08:08:29 -0500

    Add Log.php to "autoload" and change start month and duration

--- run.php
+++ run.php
@@ -4,9 +4,10 @@ require_once('Service/Engine/Engine.php');
 require_once('Service/Data/Database.php');
 require_once('Service/Data/Income.php');
 require_once('Service/Data/Expense.php');
+require_once('Service/Log.php');
 
 use Service\Engine;
 
 $engine = new Service\Engine\Engine();
-$engine->run(2026, 2, 60);
+$engine->run(2026, 1, 12);
 $engine->render();
