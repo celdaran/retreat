433ee2f2cfb847a29f48880f5ceffa2ec2b3ace7
Charlie Hills <github.cb25@tmx.email>
2023-04-29 14:11:42 -0500

    Update run.php to reflect recent changes

--- run.php
+++ run.php
@@ -8,5 +8,10 @@ $dotenv->load();
 use App\Service\Engine\Engine;
 
 $engine = new Engine('20230426');
-$engine->run(2026, 1, 30*12);
-$engine->report();
+$success = $engine->run(2026, 1, 5*12);
+if (!$success) {
+    echo "Something went wrong. Starting audit...\n";
+    $engine->audit();
+}
+$engine->render();
+// $engine->report();
