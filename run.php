e85fa644d626d295465ead727b67a3553ee15c37
Charlie Hills <github.cb25@tmx.email>
2023-04-29 23:38:55 -0500

    Add CLImate to run and introduce cli arguments for expense and assets
    Finally: don't have to edit my script to change scenarios. Note: this
    implementation is a first stab. It will likely change as it grows. I
    guess at the point where "run.php" no longer makes any sense.

--- run.php
+++ run.php
@@ -2,12 +2,41 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+use App\Service\Engine\Engine;
+use League\CLImate\CLImate;
+
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-use App\Service\Engine\Engine;
+// climate stuff TODO: MOVE SOMEWHERE
 
-$engine = new Engine('20230426');
+$climate = new CLImate();
+$climate->arguments->add(
+    [
+        'expense' => [
+            'prefix' => 'e',
+            'longPrefix' => 'expense',
+            'description' => 'Specify the name of the expense scenario',
+            'defaultValue' => 'base',
+        ],
+        'asset' => [
+            'prefix' => 'a',
+            'longPrefix' => 'asset',
+            'description' => 'Specify the name of the asset scenario',
+            'defaultValue' => 'defaults to expense scenario',
+        ],
+    ]
+);
+$climate->arguments->parse();
+
+$expenseScenario = $climate->arguments->get('expense');
+$assetScenario = $climate->arguments->get('asset');
+
+if ($assetScenario === 'defaults to expense scenario') {
+    $assetScenario = $expenseScenario;
+}
+
+$engine = new Engine($expenseScenario, $assetScenario);
 $success = $engine->run(2026, 1, 5*12);
 if (!$success) {
     echo "Something went wrong. Starting audit...\n";
