618c35c5fd5e5011083519a4a8961b76d1ce3391
Charlie Hills <github.cb25@tmx.email>
2023-05-06 10:34:50 -0500

    Commit Object Refactoring Part 1: Period class
    Instead of managing and passing around $year and $month scalars all over
    the place, I'm introducing a Period object which has three members:
    year, month, and a currentPeriod tracker. (The third isn't always used
    but can be useful).
    
    There's basic getters but also two methods that can do period math:
    
    * advance() which simply bumps the current period by one
    * addMonths() which is more general purpose, and can take any arbitrary
      year and month as arguments, so it's not tied to $this (but it's also
      not exactly static either: mostly because it shares the same internal
      logic for incrementing year+month)

--- src/Service/Engine/Util.php
+++ src/Service/Engine/Util.php
@@ -20,28 +20,4 @@ class Util {
         return round($v - $p, 2);
     }
 
-    /**
-     * Add a number of months to a year/month and return a new year/month
-     */
-    public function addMonths(int $year, int $month, int $count)
-    {
-        // First move years ahead by an integer number of years
-        $newYear = $year + intdiv($count, 12);
-
-        // Then add modulus of above as months
-        $newMonth = $month + $count % 12;
-
-        // If we went over, then roll to the next year
-        if ($newMonth > 12) {
-            $newMonth -= 12;
-            $newYear++;
-        }
-
-        // Return a structure
-        return [
-            'year' => $newYear,
-            'month' => $newMonth,
-        ];
-    }
-
 }
