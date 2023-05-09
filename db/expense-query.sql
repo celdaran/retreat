77d4256789fa05f5275e5993975a8e9349a4a168
Charlie Hills <github.cb25@tmx.email>
2023-05-09 00:52:03 -0500

    Factor SQL statements to their own files and out of the PHP code

---
+++ db/expense-query.sql
@@ -0,0 +1,31 @@
+SELECT
+  e.expense_name AS expense_name,
+  SUBSTRING_INDEX(group_concat(e.amount ORDER BY e.expense_id), ',', -1) AS amount,
+  SUBSTRING_INDEX(group_concat(e.inflation_rate ORDER BY e.inflation_rate), ',', -1) AS inflation_rate,
+  SUBSTRING_INDEX(group_concat(e.begin_year ORDER BY e.expense_id), ',', -1) AS begin_year,
+  SUBSTRING_INDEX(group_concat(e.begin_month ORDER BY e.expense_id), ',', -1) AS begin_month,
+  SUBSTRING_INDEX(group_concat(e.end_year ORDER BY e.expense_id), ',', -1) AS end_year,
+  SUBSTRING_INDEX(group_concat(e.end_month ORDER BY e.expense_id), ',', -1) AS end_month,
+  SUBSTRING_INDEX(group_concat(e.repeat_every ORDER BY e.expense_id), ',', -1) AS repeat_every,
+  SUBSTRING_INDEX(group_concat(e.fixed_period ORDER BY e.expense_id), ',', -1) AS fixed_period
+FROM (
+       SELECT
+         e.*
+       FROM scenario s1
+              JOIN expense e ON e.scenario_id = s1.scenario_id
+       WHERE s1.scenario_name = :scenario_name
+         AND s1.account_type_id = 1
+       UNION
+       SELECT
+         e.*
+       FROM scenario s1
+              LEFT JOIN scenario s2 ON s1.scenario_parent_id = s2.scenario_id
+              JOIN expense e ON e.scenario_id = s2.scenario_id
+       WHERE s1.scenario_name = :scenario_name
+         AND s1.account_type_id = 1
+
+       ORDER BY expense_id
+     ) AS e
+GROUP BY e.expense_name
+ORDER BY e.expense_name
+;
--- src/Service/Data/AssetCollection.php
+++ src/Service/Data/AssetCollection.php
@@ -206,39 +206,7 @@ class AssetCollection extends Scenario
 
     private function fetchQuery(): string
     {
-        return "
-            SELECT
-                a.asset_id,
-                a.asset_name,
-                SUBSTRING_INDEX(group_concat(a.opening_balance ORDER BY a.asset_id), ',', -1) AS opening_balance,
-                SUBSTRING_INDEX(group_concat(a.opening_balance ORDER BY a.asset_id), ',', -1) AS current_balance,
-                SUBSTRING_INDEX(group_concat(a.max_withdrawal ORDER BY a.asset_id), ',', -1) AS max_withdrawal,
-                SUBSTRING_INDEX(group_concat(a.apr ORDER BY a.asset_id), ',', -1) AS apr,
-                SUBSTRING_INDEX(group_concat(a.begin_after ORDER BY a.asset_id), ',', -1) AS begin_after,
-                SUBSTRING_INDEX(group_concat(a.begin_year ORDER BY a.asset_id), ',', -1) AS begin_year,
-                SUBSTRING_INDEX(group_concat(a.begin_month ORDER BY a.asset_id), ',', -1) AS begin_month,
-                SUBSTRING_INDEX(group_concat(a.fixed_period ORDER BY a.asset_id), ',', -1) AS fixed_period
-            FROM (
-                SELECT
-                    a.*
-                FROM scenario s1
-                JOIN asset a ON a.scenario_id = s1.scenario_id
-                WHERE s1.scenario_name = :scenario_name
-                AND s1.account_type_id = 2
-                UNION
-                SELECT
-                    a.*
-                FROM scenario s1
-                LEFT JOIN scenario s2 ON s1.scenario_parent_id = s2.scenario_id
-                JOIN asset a ON a.scenario_id = s2.scenario_id
-                WHERE s1.scenario_name = :scenario_name
-                AND s1.account_type_id = 2
-            
-                ORDER BY asset_id
-            ) AS a
-            GROUP BY a.asset_id, a.asset_name
-            ORDER BY a.asset_id, a.asset_name
-        ";
+        return file_get_contents(__DIR__ . '/../../../db/asset-query.sql');
     }
 
     private function transform(array $rows): array
